029ef1cafb1c892021720e129ec96e552c7691e5
Michal Duba <m.duba.work@gmail.com>
2024-03-18 21:51:20 +0100

    Most of the Homework is finished

--- cv3/solution.py
+++ cv3/solution.py
@@ -70,7 +70,24 @@ class MyApp:
       self.selected_index = self.notebook.index("current")
       
       # Access the tab title using the index (optional)
-      selected_tab_title = self.notebook.tab(selected_index, "text")
+      selected_tab_title = self.notebook.tab(self.selected_index, "text")
+    def expandSearch(self):
+        if(self.deleteOptions):
+            self.searchStudentLabel.destroy()
+            self.searchStudentEntry.destroy()
+            self.searchSchoolLabel.destroy()
+            self.searchSchoolEntry.destroy()
+            self.deleteOptions = False
+        else:  
+            self.searchStudentLabel = ttk.Label(self.searchOptionsFrame, text="Jméno studenta:")
+            self.searchStudentLabel.grid(row = 1, column = 0)
+            self.searchStudentEntry = ttk.Entry(self.searchOptionsFrame)
+            self.searchStudentEntry.grid(row = 1, column = 1)
+            self.searchSchoolLabel = ttk.Label(self.searchOptionsFrame, text="Jméno školy:")
+            self.searchSchoolLabel.grid(row = 2, column = 0)
+            self.searchSchoolEntry = ttk.Entry(self.searchOptionsFrame)
+            self.searchSchoolEntry.grid(row = 2, column = 1)
+            self.deleteOptions = True
     def __init__(self, root):
         root.geometry("850x500")
         root.title('Administrace')
@@ -79,30 +96,43 @@ class MyApp:
         def_font.config(size=12)
         
         #Notebook part
-        self.notebook = ttk.Notebook(root)
-        self.notebook.pack()
+        
 
-        self.highschools = ttk.Frame(self.notebook, width=400, height=200)
-        self.applications = ttk.Frame(self.notebook, width=400, height=200)
-
-        self.notebook.add(self.highschools, text='High schools')
-        self.notebook.add(self.applications, text='Application Forms')
-        self.notebook.bind("<ButtonRelease-1>", self.get_selected_notebook)
+        self.searchFrame = ttk.Frame(root)
+        self.searchResetBtn = ttk.Button(self.searchFrame, text="Reset")
+        self.searchResetBtn.pack(side="right", padx=10)
+        self.searchBtn = ttk.Button(self.searchFrame, text="Vyhledej")
+        self.searchBtn.pack(side="right", padx = 10)
+        self.searchOptionsFrame = ttk.Frame(self.searchFrame)
+        self.searchEntry = ttk.Entry(self.searchOptionsFrame, text="", width=5)
+        self.searchEntry.grid(row = 0, column = 1, sticky="w")
+        self.searchLabel = ttk.Label(self.searchOptionsFrame, text="Číslo přihlášky:")
+        self.searchLabel.grid(row = 0, column = 0)
+        self.searchOptionsFrame.pack(side = "right")
+        self.expandSearchBtn = ttk.Button(self.searchFrame, text="Více", width=5, command=self.expandSearch)
+        self.deleteOptions = False
+        self.expandSearchBtn.pack(side = "right", padx=5)
+        self.searchFrame.pack(fill="both", pady=20)
+        self.applications = ttk.Frame(root, width=400, height=200)
+        self.applications.pack()
+        #self.notebook.add(self.applications, text='High schools')
+        #self.notebook.add(self.applications, text='Application Forms')
+        #self.notebook.bind("<ButtonRelease-1>", self.get_selected_notebook)
         #Search bar part
-        self.searchEntry = ttk.Entry(self.highschools, text="Název školy: ")
+        
         
         #Table part - Highschool
-        self.tableFrame = ttk.Frame(self.highschools,borderwidth= 5)
-        self.table = ttk.Treeview(self.tableFrame, columns=('name', 'street', 'city', 'psc'), 
+        self.tableFrame = ttk.Frame(self.applications,borderwidth= 5)
+        self.table = ttk.Treeview(self.tableFrame, columns=('cp', 'datum_podani', 'jmeno_studenta', 'jmeno_skoly'), 
                             show='headings')
 
-        self.table.heading('name', text='School name')
-        self.table.heading('street', text='Street')
-        self.table.heading('city', text='City')
-        self.table.heading('psc', text='PSC')
+        self.table.heading('cp', text='Číslo přihlášky')
+        self.table.heading('datum_podani', text='Datum podání')
+        self.table.heading('jmeno_studenta', text='Jméno Studenta')
+        self.table.heading('jmeno_skoly', text='Název školy')
 
-        self.table.insert('', END, values=("VŠB", "17. listopadu 2172/15", "Ostrava", "708 00"))
-        self.table.insert('', END, values=("Ostravská Univerzita", "Dvořákova 138/7", "Ostrava","701 03"))
+        self.table.insert('', END, values=("001", "18/03/2024", "Jiří Matonoha", "SPSEI Ostrava"))
+        self.table.insert('', END, values=("002", "18/03/2024", "Jiří Matonoha","Telekomka Ostrava"))
         self.table.bind('<<TreeviewSelect>>', self.item_selected)
         self.table.grid(row=0, column=0, sticky='nsew')
 
@@ -110,6 +140,7 @@ class MyApp:
         self.table.configure(yscroll=scrollbar.set)
         scrollbar.grid(row=0, column=1, sticky='ns')
         self.tableFrame.pack(side="top")
+        self.applications.pack(side="top")
         
         #Menu part
         self.menubar = Menu(root)
@@ -119,22 +150,30 @@ class MyApp:
 
         root.config(menu=self.menubar)
         
-        #Form part
+        #Bottom part
         self.bottomPartFrame = ttk.Frame(root)
-        self.infoFrameHS = ttk.Frame(self.bottomPartFrame)
-        for i in range(0,4):
-            label = ttk.Label(self.infoFrameHS, text="Název: ")
-            entry = ttk.Entry(self.infoFrameHS, text="")
-            label.grid(row = i, column = 0)
-            entry.grid(row = i, column= 1)
-        self.infoFrameAF = ttk.Frame(self.bottomPartFrame)
+        self.notebook = ttk.Notebook(root)
+        self.notebook.pack(fill="both", expand=True)
+        self.infoFrameAF = ttk.Frame(self.notebook, width=400, height=200)
+        self.AFLabelNumber = ttk.Label(self.infoFrameAF, text="Číslo přihlášky")
+        self.AFLabelNumber.grid(row = 0, column = 0)
+
+        self.infoFrameStudent = ttk.Frame(self.notebook, width=400, height=200)
+        self.SLabelNumber = ttk.Label(self.infoFrameStudent, text="Jméno studenta")
+        self.SLabelNumber.grid(row = 0, column = 0)
+        self.infoFrameHS = ttk.Frame(self.notebook, width=400, height=200)
+        self.HSLabelNumber = ttk.Label(self.infoFrameHS, text="Název školy")
+        self.HSLabelNumber.grid(row = 0, column = 0)
+        
+        self.notebook.add(self.infoFrameAF, text='Podrobnosti')
+        self.notebook.add(self.infoFrameStudent, text='Student')
+        self.notebook.add(self.infoFrameHS, text='Škola')
         
         self.controlFrame = ttk.Frame(self.bottomPartFrame, borderwidth = 5)
         self.insertButton = ttk.Button(self.controlFrame, text="Nový záznam", command=lambda: self.createFormWindow(root))
         self.insertButton.grid(row = 0, column = 0)
-        self.controlFrame.pack(side="left", anchor="nw")
-        self.infoFrameHS.pack(side="right", anchor="ne")
-        self.bottomPartFrame.pack()
+        self.controlFrame.pack(side="left", anchor="w", fill="both", expand=True)
+        self.bottomPartFrame.pack(side = "bottom")
         #self.formWindow.grab_set()
 root = Tk()
 myApp = MyApp(root)
